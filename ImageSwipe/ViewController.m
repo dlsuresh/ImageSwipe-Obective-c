61304c109a7503e07a1db2e8f542468398538b85
dlsuresh <dl.suresh6@gmail.com>
2017-03-07 15:40:56 +0530

    initial commit

--- ImageSwipe/ViewController.m
+++ ImageSwipe/ViewController.m
@@ -9,21 +9,81 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong , nonatomic)NSArray *imgArray;
+
+//-(void )SwipeGestureLeft:(UISwipeGestureRecognizer *)Gesture;
+//-(void )SwipeGestureRight:(UISwipeGestureRecognizer *)Gesture;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSInteger index;;
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imgArray = @[@"image1.jpg",@"image2.jpg",@"images3.jpg",@"images4.jpg"];
+    index=0;
+//    self.img.image = [UIImage imageNamed:[self.imgArray objectAtIndex:index]];
+//    UISwipeGestureRecognizer *RightSwipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(SwipeGestureRight:)];
+//    RightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
+//    UISwipeGestureRecognizer *LeftSwipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(SwipeGestureLeft:)];
+//    LeftSwipe.direction = UISwipeGestureRecognizerDirectionLeft;
+//
+//    [self.img addGestureRecognizer:RightSwipe];
+    //[self.img addGestureRecognizer:LeftSwipe];
+    
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
 }
+- (IBAction)SwipeRight:(UISwipeGestureRecognizer *)sender {
+    index++;
+    if (index > self.imgArray.count-1) {
+        index=0;
+    }
+    
+    self.img.image = [UIImage imageNamed:[self.imgArray objectAtIndex:index]];
+    
+
+}
+- (IBAction)SwipeLeft:(UISwipeGestureRecognizer *)sender {
+    index--;
+
+    if (index < 0) {
+        index = self.imgArray.count-1;
+    }
+    if (index > _imgArray.count-1) {
+        index =0;
+    }
+    self.img.image = [UIImage imageNamed:[self.imgArray objectAtIndex:index]];
+    }
+
+//-(void )SwipeGestureLeft:(UISwipeGestureRecognizer *)Gesture{
+//    if (index < 0) {
+//        index = self.imgArray.count-1;
+//    }
+//    self.img.image = [UIImage imageNamed:[self.imgArray objectAtIndex:index]];
+//    index--;
+//}
+//-(void )SwipeGestureRight:(UISwipeGestureRecognizer *)Gesture{
+//    if (index > self.imgArray.count-1) {
+//        index=0;
+//    }
+//    
+//    self.img.image = [UIImage imageNamed:[self.imgArray objectAtIndex:index]];
+//    index++;
+//
+//}
 
 
 @end
